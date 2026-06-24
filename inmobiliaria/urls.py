ff4a2712ff66013013ce2e200f6b8830f455ad93
fbressan-itec <f.bressannadal@itecriocuarto.org.ar>
2026-06-24 14:50:20 -0300

    Creada detalle de propiedades, ruta, galeria de fotos en Ver Mas

--- inmobiliaria/urls.py
+++ inmobiliaria/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.lista_propiedades, name='lista_propiedades'),
+    path('propiedad/<int:pk>/', views.detalle_propiedad, name='detalle_propiedad'), #url de la propiedad por id/pk
 ]
--- inmobiliaria/views.py
+++ inmobiliaria/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Propiedad
 
+# lista de propiedades
 def lista_propiedades(request):
     # traer las propiedades de la bd
     propiedades = Propiedad.objects.all()
     # las paos al template inmobiliaria
     return render(request, 'inmobiliaria/lista.html', {'propiedades': propiedades})
+
+# "ver mas" detalles de cada propiedad
+def detalle_propiedad(request, pk):
+    # get o 404 por id/pk
+    propiedad = get_object_or_404(Propiedad, pk=pk)
+    return render(request, 'inmobiliaria/detalle.html', {'propiedad': propiedad})
